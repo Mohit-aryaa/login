23d1790c1d2d2f1a83903f04167a8cdbbd2177c7
Shivam Ratta <shivamratta.7@gmail.com>
2021-10-13 09:34:39 +0530

    Login form and employee data table added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
+  {path: 'data' , loadChildren: () => import('./employee-data/employee-data.module').then(m => m.EmployeeDataModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
